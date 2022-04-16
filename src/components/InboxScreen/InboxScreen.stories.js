7531e6600d26b17f9d3ce75c3d0eb538ec9e23b7
thisnamewasnottaken <8645373+thisnamewasnottaken@users.noreply.github.com>
2022-04-16 18:22:35 +0100

    Updated interactivity

--- src/components/InboxScreen/InboxScreen.stories.js
+++ src/components/InboxScreen/InboxScreen.stories.js
@@ -7,6 +7,13 @@ import { MockedState } from "./../TaskList/TaskList.stories";
 
 import { Provider } from "react-redux";
 
+import {
+  fireEvent,
+  within,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@storybook/testing-library";
+
 export default {
   component: InboxScreen,
   title: "InboxScreen",
@@ -40,4 +47,16 @@ Error.parameters = {
       ),
     ],
   },
+};
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Waits for the component to transition from the loading state
+  await waitForElementToBeRemoved(await canvas.findByTestId("loading"));
+  // Waits for the component to be updated based on the store
+  await waitFor(async () => {
+    // Simulates pinning the first task
+    await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+    // Simulates pinning the third task
+    await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+  });
 };
