5da2ca6e4c11a768e0b4a7eb51b5cde85b0a16f3
thisnamewasnottaken <8645373+thisnamewasnottaken@users.noreply.github.com>
2022-04-16 18:12:50 +0100

    Wired in data

---
+++ src/components/InboxScreen/InboxScreen.stories.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import InboxScreen from "../InboxScreen/InboxScreen";
+import store from "../../lib/store";
+import { rest } from "msw";
+import { MockedState } from "./../TaskList/TaskList.stories";
+
+import { Provider } from "react-redux";
+
+export default {
+  component: InboxScreen,
+  title: "InboxScreen",
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+};
+
+const Template = () => <InboxScreen />;
+
+export const Default = Template.bind({});
+Default.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=1",
+        (req, res, ctx) => {
+          return res(ctx.json(MockedState.tasks));
+        }
+      ),
+    ],
+  },
+};
+export const Error = Template.bind({});
+Error.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=1",
+        (req, res, ctx) => {
+          return res(ctx.status(403));
+        }
+      ),
+    ],
+  },
+};
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,9 +1,8 @@
 import React from "react";
-import PropTypes from "prop-types";
 import Task from "../Task/Task";
 
 import { useDispatch, useSelector } from "react-redux";
-import { updateTaskState } from "../lib/store";
+import { updateTaskState } from "../../lib/store"
 
 export default function TaskList() {
   // We're retrieving our state from the store
@@ -75,17 +74,3 @@ export default function TaskList() {
     </div>
   );
 }
-
-TaskList.propTypes = {
-  /** Checks if it's in loading state */
-  loading: PropTypes.bool,
-  /** The list of tasks */
-  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
-  /** Event to change the task to pinned */
-  onPinTask: PropTypes.func,
-  /** Event to change the task to archived */
-  onArchiveTask: PropTypes.func,
-};
-TaskList.defaultProps = {
-  loading: false,
-};
